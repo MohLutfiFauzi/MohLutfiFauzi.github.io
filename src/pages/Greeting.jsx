89a33db9285248dc5ae6cd260123a0840db7ec3d
mlfauzi54@gmail.com <mlfauzi54@gmail.com>
2022-10-24 00:00:31 +0700

    change font size in greeting

--- src/pages/Greeting.jsx
+++ src/pages/Greeting.jsx
@@ -2,21 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import 'animate.css';
 
-const flCon = {
-  size: 2, // in vw
-  max: 36, // in px
-  min: 24 // in px
-};
-
-const flCon2 = {
-  size: 2, // in vw
-  max: 24, // in px
-  min: 14 // in px
-};
-
-const calcMinBreak = (size, min) => Math.round((min * 100) / size);
-const calcMaxBreak = (size, max) => Math.round((max * 100) / size);
-
 const Container = styled.main`
   padding: 1rem;
   background-color: #1E1E1E;
@@ -48,33 +33,17 @@ const Container = styled.main`
 `
 
 const Greet = styled.h1`
-  font-size: ${flCon.size}vw;
+  font-size: 36px;
   font-weight: 500;
   color: white;
   text-align: center;
-
-  @media (max-width: ${calcMinBreak(flCon.size, flCon.min)}px) {
-    font-size: ${flCon.min}px;
-  }
-
-  @media (min-width: ${calcMaxBreak(flCon.size, flCon.max)}px) {
-    font-size: ${flCon.max}px;
-  }
 `
 
 const Desc = styled.p`
-  font-size: ${flCon2.size}vw;
+  font-size: 24px;
   color: white;
   font-weight: 400;
   text-align: center;
-
-  @media (max-width: ${calcMinBreak(flCon2.size, flCon2.min)}px) {
-    font-size: ${flCon2.min}px;
-  }
-
-  @media (min-width: ${calcMaxBreak(flCon2.size, flCon2.max)}px) {
-    font-size: ${flCon2.max}px;
-  }
 `
 
 const Tag = styled.span`
